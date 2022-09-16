01b6dcc959505782f84688c25a229d9e9165ae6c
Suk Jang Moon <73870711+mookee2809@users.noreply.github.com>
2022-09-16 15:50:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "54.90.212.23",
+                  "54.160.156.110",
                   "db_admin",
                   "rmit_password",
                   "rmit_store_db"
